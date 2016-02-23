d03081d9e51811a91d1f05fb7ffb837da27f8eb9
segap <shanemcgov@gmail.com>
2016-02-23 09:47:17 +0000

    changing the propagation script so it'll use zip provided by GIT - adding force overwrite

--- prop.sh
+++ prop.sh
@@ -2,5 +2,5 @@ cd /var/www/htdocs/
 rm -f master.zip
 wget http://bryantlab02.rtp.raleigh.ibm.com/public/master.zip
 mkdir -p /var/www/htdocs/selfservice/
-unzip master.zip -d /var/www/htdocs/selfservice/
+unzip -o master.zip -d /var/www/htdocs/selfservice/
 chown -R apache:apache selfservice/
