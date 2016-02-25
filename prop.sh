397532b4f4edc4c9d7b2b0a91d263519d99901d6
Pages <shanmcgo@ie.ibm.com>
2016-02-25 13:14:41 +0000

    adding the DB Queries text file to creation script

--- prop.sh
+++ prop.sh
@@ -1,6 +1,10 @@
 cd /var/www/htdocs/
 rm -f master.zip
+if [ ! -f /tmp/foo.txt ]; then
+    touch QueryLogging.txt
+fi
 wget http://bryantlab02.rtp.raleigh.ibm.com/public/master.zip
 mkdir -p /var/www/htdocs/selfservice/
 unzip -o master.zip -d /var/www/htdocs/selfservice/
 chown -R apache:apache selfservice/
+chown apache:apache QueryLogging.txt
