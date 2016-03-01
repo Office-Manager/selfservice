38dbd326404824aa3b919d1d68f4583c8a155bad
Pages <shanmcgo@ie.ibm.com>
2016-03-01 09:48:10 +0000

    Adding the remove directory to prop.sh

--- prop.sh
+++ prop.sh
@@ -4,6 +4,7 @@ if [ ! -f /tmp/foo.txt ]; then
     touch QueryLogging.txt
 fi
 wget http://bryantlab02.rtp.raleigh.ibm.com/public/master.zip
+rm -rf /var/www/htdocs/selfservice/
 mkdir -p /var/www/htdocs/selfservice/
 unzip -o master.zip -d /var/www/htdocs/selfservice/
 chown -R apache:apache selfservice/
