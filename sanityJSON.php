6a6937d92a5787c410da875c4bd41af96dd449a8
Peter Poliwoda <peterpoliwoda@ie.ibm.com>
2016-06-02 16:30:11 +0100

    Sanity fix

--- sanityJSON.php
+++ sanityJSON.php
@@ -1,13 +1,14 @@
 <?php
     header('Content-Type: application/json');
 
-    if (file_exists("/var/www/htdocs/sales/salesconnect/custom/service/IBMSanityCheck/serverStatusJSON.php")) {
-        $value = shell_exec('php custom/service/IBMSanityCheck/serverStatusJSON.php');
-        $value = substr($value,28);
-         }
+    chdir('/var/www/htdocs/sales/salesconnect/');
+    if (file_exists("/var/www/htdocs/sales/salesconnect/custom/service/IBMSanityCheck/serverStatusJSON.php")) {  
+      $value = shell_exec('php custom/service/IBMSanityCheck/serverStatusJSON.php');
+      $value = substr($value,28);
+    }
     else {
 	    $value = shell_exec('php serverStatusJSON.php');
-		$value = substr($value,28);
+		  $value = substr($value,28);
          }
     $value = substr($value,0,-1);
     echo $value;
